f2f74de752d2614d5aa76d80e46b2a477e323c6e
Md.Dulal Miah <dulalmiah70112@gmail.com>
2021-03-02 12:10:46 +0600

    ready data

---
+++ src/fakeData/fakeData.js
@@ -0,0 +1,169 @@
+const fakeData = [
+                {
+                    id : 'bs01',
+                    category : 'lunch',
+                    name : 'Healthy Meal plan',
+                    image : "https://i.ibb.co/QJwQ9Hb/lunch1.png" ,
+                    price : 9,
+                    description : "Lorem ipsum, dolor sit amet consectetur adipisicing elit. Reprehenderit, similique?",
+                    details : "Lorem ipsum dolor sit, amet consectetur adipisicing elit. Exercitationem, quod sint! Quibusdam corporis quia, ab consequuntur tempora fugit ducimus, totam voluptatum ipsa deserunt est minus molestias pariatur hic quos praesentium natus dolorem nulla culpa reiciendis nisi suscipit distinctio cupiditate dolor! Libero eveniet illum aliquam in nam incidunt nihil labore sequi blanditiis est"
+                },
+                {
+                    id : 'bs02',
+                    category : 'lunch',
+                    name : 'Fried Chicken Bento',
+                    image : "https://i.ibb.co/TP1ZTj9/lunch2.png" ,
+                    price : 6,
+                    description : "Lorem ipsum, dolor sit amet consectetur adipisicing elit. Reprehenderit, similique?",
+                    details : "Lorem ipsum dolor, sit amet consectetur adipisicing elit. Fuga, asperiores! Error at commodi voluptatum et fugit ratione nisi. Corrupti consequuntur repudiandae minus nam fuga officia cumque inventore modi, quam ipsa!"
+                },
+                {
+                    id : 'bs03',
+                    category : 'lunch',
+                    name : 'Tarragon-Rubbed-salman',
+                    image : "https://i.ibb.co/Y7RctZF/lunch3.png",
+                    price : 5,
+                    description : "Lorem ipsum, dolor sit amet consectetur adipisicing elit. Reprehenderit, similique?",
+                    details : "Lorem ipsum dolor sit amet consectetur adipisicing elit. Quibusdam blanditiis illum reprehenderit, ea, temporibus maxime expedita sit nam porro veniam ducimus quos facilis laboriosam rem recusandae esse alias id culpa."
+                },
+                {
+                    id : 'bs04',
+                    category : 'lunch',
+                    name : 'Indian Lunch',
+                    image : "https://i.ibb.co/fSNSzht/lunch4.png",
+                    price : 4,
+                    description : "Lorem ipsum, dolor sit amet consectetur adipisicing elit. Reprehenderit, similique?",
+                    details : "Lorem ipsum dolor sit amet consectetur adipisicing elit. Eveniet modi laboriosam autem soluta quam eum officia vel! Repellendus commodi quaerat veritatis esse quia in amet distinctio, corrupti velit aliquid nulla?"
+                },
+                {
+                    id : 'bs05',
+                    category : 'lunch',
+                    name : 'Honey-soy-Glazed salman',
+                    image : "https://i.ibb.co/Lt4xdcW/lunch5.png" ,
+                    price : 5,
+                    description : "Lorem ipsum, dolor sit amet consectetur adipisicing elit. Reprehenderit, similique?",
+                    details : "Lorem ipsum dolor sit amet consectetur adipisicing elit. Doloribus nesciunt sapiente error aut odio laborum mollitia voluptatum, ut, repudiandae nobis, similique voluptates quas magnam amet ex reiciendis tempore aliquam. Molestiae."
+                },
+                {
+                    id : 'bs06',
+                    category : 'lunch',
+                    name : 'Beet Steck',
+                    image : "https://i.ibb.co/qFrjR33/lunch6.png" ,
+                    price : 20,
+                    description : "Lorem ipsum, dolor sit amet consectetur adipisicing elit. Reprehenderit, similique?",
+                    details : "Lorem, ipsum dolor sit amet consectetur adipisicing elit. Minima, omnis repellat? Error perferendis veritatis accusantium tempora obcaecati quos adipisci voluptatibus? Placeat aliquam ipsa consequuntur in reprehenderit exercitationem minima tempore nesciunt."
+                }
+                ,
+                {
+                    id : 'bs07',
+                    category : 'breakfast',
+                    name : 'Bagel and Cream Cheese',
+                    image : "https://i.ibb.co/x3qWs10/breakfast1.png" , 
+                    price : 30.99,
+                    description : "Lorem ipsum, dolor sit amet consectetur adipisicing elit. Reprehenderit, similique?",
+                    details : "Lorem, ipsum dolor sit amet consectetur adipisicing elit. Minima, omnis repellat? Error perferendis veritatis accusantium tempora obcaecati quos adipisci voluptatibus? Placeat aliquam ipsa consequuntur in reprehenderit exercitationem minima tempore nesciunt."
+                },
+                {
+                    id : 'bs08',
+                    category : 'breakfast',
+                    name : 'Breakfast Sandwich',
+                    image : "https://i.ibb.co/CQMbdb1/breakfast2.png" , 
+                    price : 15.99,
+                    description : "Lorem ipsum, dolor sit amet consectetur adipisicing elit. Reprehenderit, similique?",
+                    details : "Lorem, ipsum dolor sit amet consectetur adipisicing elit. Minima, omnis repellat? Error perferendis veritatis accusantium tempora obcaecati quos adipisci voluptatibus? Placeat aliquam ipsa consequuntur in reprehenderit exercitationem minima tempore nesciunt."
+                },
+                {
+                    id : 'bs09',
+                    category : 'breakfast',
+                    name : 'Baked Chicken',
+                    image : "https://i.ibb.co/DMF9kdC/breakfast3.png",
+                    price : 4,
+                    description : "Lorem ipsum dolor, sit amet consectetur adipisicing elit. Atque, tempora.",
+                    details : "Lorem, ipsum dolor sit amet consectetur adipisicing elit. Suscipit unde ipsa asperiores odio nulla. Alias, exercitationem! Ut atque eius odit nihil necessitatibus? Corrupti alias possimus blanditiis debitis itaque laboriosam magnam."
+                },
+                {
+                    id : 'bs10',
+                    category : 'breakfast' , 
+                    name : 'Eggs Benedict',
+                    image : "https://i.ibb.co/6ys7YWg/breakfast4.png" ,
+                    price : 4.99,
+                    description : "Lorem ipsum dolor, sit amet consectetur adipisicing elit. Atque, tempora.",
+                    details : "Lorem, ipsum dolor sit amet consectetur adipisicing elit. Suscipit unde ipsa asperiores odio nulla. Alias, exercitationem! Ut atque eius odit nihil necessitatibus? Corrupti alias possimus blanditiis debitis itaque laboriosam magnam."
+                },
+                {
+                    id : 'bs11',
+                    category : 'breakfast' , 
+                    name : 'Toast Crossiant Fried Eggs',
+                    image : "https://i.ibb.co/sWMNMkP/breakfast5.png" , 
+                    price : 10.99,
+                    description : "Lorem ipsum dolor, sit amet consectetur adipisicing elit. Atque, tempora.",
+                    details : "Lorem ipsum dolor sit amet consectetur adipisicing elit. Eaque deleniti possimus omnis quis sint, in necessitatibus accusantium debitis natus officiis est alias reiciendis magni modi temporibus rerum maiores deserunt quidem."
+                },
+                {
+                    id : 'bs12',
+                    category : 'breakfast' , 
+                    name : 'Full Breakfast Fried Egg Toast burek' , 
+                    image : "https://i.ibb.co/t8CMsxt/breakfast6.png" ,
+                    price : 7.99,
+                    description : "Lorem ipsum dolor, sit amet consectetur adipisicing elit. Atque, tempora.",
+                    details : "Lorem ipsum dolor, sit amet consectetur adipisicing elit. Consequuntur reiciendis iste soluta earum recusandae? Nesciunt tempora, ratione ipsa distinctio quo sequi? Accusantium atque velit sapiente et in, enim quis deleniti?"
+                },
+                {
+                    id : 'bs13',
+                    category : 'dinner' , 
+                    name : 'Salmon with Grape Fruit and Until salad',
+                    image : "https://i.ibb.co/3myzWRY/dinner1.png" ,
+                    price : 8.99,
+                    description : "Lorem ipsum dolor, sit amet consectetur adipisicing elit. Atque, tempora.",
+                    details : "Lorem ipsum dolor sit amet consectetur adipisicing elit. Ex magni quis odio officia tenetur sed adipisci dicta, qui, explicabo iusto natus tempore nulla possimus quaerat sunt, quos illo cupiditate magnam!"
+                },
+                {
+                    id : 'bs14',
+                    category : 'dinner' , 
+                    name : 'Lemony salmon Piccata',
+                    image : "https://i.ibb.co/LgtkYHH/dinner2.png" ,
+                    price : 5.99 , 
+                    description : "Lorem ipsum dolor, sit amet consectetur adipisicing elit. Atque, tempora.",
+                    details : "Lorem ipsum dolor sit amet consectetur adipisicing elit. Sapiente atque debitis, eos architecto odit rem fuga suscipit soluta molestiae doloremque ea ullam nemo iure dolorem? Perspiciatis nobis deserunt doloremque repudiandae!"
+                },
+
+                {
+                    id : 'bs15',
+                    category : 'dinner',
+                    name : 'Pork tenderloin piccata' , 
+                    image : "https://i.ibb.co/fdnVpqk/dinner3.png" ,
+                    price : 3.99,
+                    description : "Lorem ipsum dolor, sit amet consectetur adipisicing elit. Atque, tempora.",
+                    details : "Lorem ipsum dolor sit amet consectetur adipisicing elit. Nulla ab distinctio provident, ratione, eveniet ipsum atque nobis ducimus ullam, hic sit corporis dolorem mollitia cumque ipsa excepturi similique sint odit."
+                },
+                {
+                    id : 'bs16',
+                    category : 'dinner' , 
+                    name : 'Frunch Frice with cheese' , 
+                    image : "https://i.ibb.co/1vPpKGg/dinner4.png" ,
+                    price : 6.99, 
+                    description : "Lorem ipsum dolor, sit amet consectetur adipisicing elit. Atque, tempora.",
+                    details : "Lorem ipsum dolor sit amet consectetur adipisicing elit. Quod nihil velit pariatur porro nemo ducimus, debitis sequi adipisci nesciunt voluptatibus, veniam dignissimos molestias ab. Dolor soluta commodi ipsum explicabo vero!"
+                },
+                {
+                    id : 'bs17',
+                    category : 'dinner' , 
+                    name : 'Gurlic Butter with cheese', 
+                    image : "https://i.ibb.co/B24kQnD/dinner5.png" ,
+                    price  : 2.99,
+                    description : "Lorem ipsum dolor, sit amet consectetur adipisicing elit. Atque, tempora.",
+                    details : "Lorem, ipsum dolor sit amet consectetur adipisicing elit. Iusto voluptas quo totam nisi iure sit minima ex ea id sunt animi, illum at, vel voluptatum. Laboriosam modi cumque distinctio iure!"
+                } , 
+                {
+                    id : 'bs18',
+                    category : 'dinner' , 
+                    name : 'Bucket Frices' , 
+                    image : "https://i.ibb.co/WF5bjCW/dinner6.png" , 
+                    price : 10,
+                    description : "Lorem ipsum dolor, sit amet consectetur adipisicing elit. Atque, tempora.",
+                    details : "Lorem ipsum dolor sit amet consectetur adipisicing elit. Et mollitia possimus saepe tempore. Voluptatum laudantium enim aspernatur perspiciatis. Quae repudiandae sequi amet at qui error expedita nostrum laudantium voluptate est."
+                }
+
+            
+]
+export default fakeData;
